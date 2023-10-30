72398e956a83727864ddf479f3b583bd0b463653
George Padron <georgenpadron@gmail.com>
2023-10-30 20:24:52 +0000

    Rewrote mu4e config and added arch install command

--- init.el
+++ init.el
@@ -206,20 +206,27 @@ emphasis markers inside of org mode"
   (setq org-roam-directory (file-truename (concat gp/org-directory "/roam")))
   (org-roam-db-autosync-mode))
 
-;;; EMAIL
-(use-package mu4e
+;;; EMAIL(use-package mu4e
+  ;; Mu is a package installed /outside/ of emacs
   :ensure nil
   :config
   ;; This is set to 't' to avoid mail syncing issues when using mbsync
-  (setq mu4e-change-filenames-when-moving t)
+  (setq mu4e-change-filenames-when-moving t
+        mu4e-use-maildirs-extension nil)
 
   ;; Referesh mail using isync every 10 minutes
-  (setq mu4e-update-interval (* 10 60))
-  (setq mu4e-get-mail-command "mbsync -a")
-  (setq mu4e-maildir "~/Mail")
+  ;; NOTE: This is disabled in this config as this is being handled instead
+  ;; by a bash script
+  (setq mu4e-update-interval (* 10 60)
+        mu4e-get-mail-command "mbsync -a"
+        mu4e-maildir "~/.local/share/mail")
 
-  ;; Configure the function to be used for sending mail
-  (setq message-send-mail-function 'smtpmail-send-it)
+  ;; Configure mail sending to use msmtp
+  (setq sendmail-program (executable-find "msmtp")
+        send-mail-function #'smtpmail-send-it
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-send-mail-function #'message-send-mail-with-sendmail)
 
   (setq mu4e-contexts
         (list
@@ -229,22 +236,22 @@ emphasis markers inside of org mode"
           :match-func
           (lambda (msg)
             (when msg
-              (string-prefix-p "/Gmail" (mu4e-message-field msg :maildir))))
+              (string-prefix-p "/georgenpadron@gmail.com" (mu4e-message-field msg :maildir))))
           :vars '((user-mail-address . "georgenpadron@gmail.com")
                   (user-full-name . "George N Padron")
-                  (smtpmail-smtp-server . "smtp.gmail.com")
-                  (smtpmail-smtp-service . 465)
-                  (smtpmail-stream-type . ssl)
-                  (mu4e-drafts-folder . "/Gmail/[Gmail]/Drafts")
-                  (mu4e-sent-folder . "/Gmail/[Gmail]/Sent Mail")
-                  (mu4e-refile-folder . "/Gmail/[Gmail]/All Mail")
-                  (mu4e-trash-folder . "/Gmail/[Gmail]/Trash")
+                  ;; (smtpmail-smtp-server . "smtp.gmail.com")
+                  ;; (smtpmail-smtp-service . 465)
+                  ;; (smtpmail-stream-type . ssl)
+                  (mu4e-drafts-folder . "/georgenpadron@gmail.com/[Gmail]/Drafts")
+                  (mu4e-sent-folder . "/georgenpadron@gmail.com/[Gmail]/Sent")
+                  (mu4e-refile-folder . "/georgenpadron@gmail.com/[Gmail]/All Mail")
+                  (mu4e-trash-folder . "/georgenpadron@gmail.com/[Gmail]/Trash")
                   (mu4e-maildir-shortcuts .
-                                          (("/Gmail/Inbox" . ?i)
-                                           ("/Gmail/[Gmail]/Sent Mail" . ?s)
-                                           ("/Gmail/[Gmail]/Trash" . ?t)
-                                           ("/Gmail/[Gmail]/Drafts" . ?d)
-                                           ("/Gmail/[Gmail]/All Mail" . ?a)))
+                                          (("/georgenpadron@gmail.com/INBOX" . ?i)
+                                           ("/georgenpadron@gmail.com/[Gmail]/Sent Mail" . ?s)
+                                           ("/georgenpadron@gmail.com/[Gmail]/Trash" . ?t)
+                                           ("/georgenpadron@gmail.com/[Gmail]/Drafts" . ?d)
+                                           ("/georgenpadron@gmail.com/[Gmail]/All Mail" . ?a)))
                   ))
 
          ;; Wealth Account
@@ -253,48 +260,49 @@ emphasis markers inside of org mode"
           :match-func
           (lambda (msg)
             (when msg
-              (string-prefix-p "/Wealth" (mu4e-message-field msg :maildir))))
+              (string-prefix-p "/wealth2005@gmail.com" (mu4e-message-field msg :maildir))))
           :vars '((user-mail-address . "wealth2005@gmail.com")
                   (user-full-name . "George N Padron")
-                  (smtpmail-smtp-server . "smtp.gmail.com")
-                  (smtpmail-smtp-service . 465)
-                  (smtpmail-stream-type . ssl)
-                  (mu4e-drafts-folder . "/Wealth/[Gmail]/Drafts")
-                  (mu4e-sent-folder . "/Wealth/[Gmail]/Sent Mail")
-                  (mu4e-refile-folder . "/Wealth/[Gmail]/All Mail")
-                  (mu4e-trash-folder . "/Wealth/[Gmail]/Trash")
+                  ;; (smtpmail-smtp-server . "smtp.gmail.com")
+                  ;; (smtpmail-smtp-service . 465)
+                  ;; (smtpmail-stream-type . ssl)
+                  (mu4e-drafts-folder . "/wealth2005@gmail.com/[Gmail]/Drafts")
+                  (mu4e-sent-folder . "/wealth2005@gmail.com/[Gmail]/Sent Mail")
+                  (mu4e-refile-folder . "/wealth2005@gmail.com/[Gmail]/All Mail")
+                  (mu4e-trash-folder . "/wealth2005@gmail.com/[Gmail]/Trash")
                   (mu4e-maildir-shortcuts .
-                                          (("/Wealth/Inbox" . ?i)
-                                           ("/Wealth/[Gmail]/Sent Mail" . ?s)
-                                           ("/Wealth/[Gmail]/Trash" . ?t)
-                                           ("/Wealth/[Gmail]/Drafts" . ?d)
-                                           ("/Wealth/[Gmail]/All Mail" . ?a)))
+                                          (("/wealth2005@gmail.com/INBOX" . ?i)
+                                           ("/wealth2005@gmail.com/[Gmail]/Sent Mail" . ?s)
+                                           ("/wealth2005@gmail.com/[Gmail]/Trash" . ?t)
+                                           ("/wealth2005@gmail.com/[Gmail]/Drafts" . ?d)
+                                           ("/wealth2005@gmail.com/[Gmail]/All Mail" . ?a)))
                   ))
-         ;; Vanderbilt Account
+         ;; george.n.padron@vanderbilt.edu Account
          (make-mu4e-context
           :name "Vanderbilt"
           :match-func
           (lambda (msg)
             (when msg
-              (string-prefix-p "/Vanderbilt" (mu4e-message-field msg :maildir))))
+              (string-prefix-p "/george.n.padron@vanderbilt.edu" (mu4e-message-field msg :maildir))))
           :vars '((user-mail-address . "george.n.padron@vanderbilt.edu")
                   (user-full-name . "George N Padron")
                   (smtpmail-smtp-server . "smtp.gmail.com")
                   (smtpmail-smtp-service . 465)
                   (smtpmail-stream-type . ssl)
-                  (mu4e-drafts-folder . "/Vanderbilt/[Gmail]/Drafts")
-                  (mu4e-sent-folder . "/Vanderbilt/[Gmail]/Sent Mail")
-                  (mu4e-refile-folder . "/Vanderbilt/[Gmail]/All Mail")
-                  (mu4e-trash-folder . "/Vanderbilt/[Gmail]/Trash")
+                  (mu4e-drafts-folder . "/george.n.padron@vanderbilt.edu/[Gmail]/Drafts")
+                  (mu4e-sent-folder . "/george.n.padron@vanderbilt.edu/[Gmail]/Sent Mail")
+                  (mu4e-refile-folder . "/george.n.padron@vanderbilt.edu/[Gmail]/All Mail")
+                  (mu4e-trash-folder . "/george.n.padron@vanderbilt.edu/[Gmail]/Trash")
                   (mu4e-maildir-shortcuts .
-                                          (("/Vanderbilt/Inbox" . ?i)
-                                           ("/Vanderbilt/[Gmail]/Sent Mail" . ?s)
-                                           ("/Vanderbilt/[Gmail]/Trash" . ?t)
-                                           ("/Vanderbilt/[Gmail]/Drafts" . ?d)
-                                           ("/Vanderbilt/[Gmail]/All Mail" . ?a)))
+                                          (("/george.n.padron@vanderbilt.edu/INBOX" . ?i)
+                                           ("/george.n.padron@vanderbilt.edu/[Gmail]/Sent Mail" . ?s)
+                                           ("/george.n.padron@vanderbilt.edu/[Gmail]/Trash" . ?t)
+                                           ("/george.n.padron@vanderbilt.edu/[Gmail]/Drafts" . ?d)
+                                           ("/george.n.padron@vanderbilt.edu/[Gmail]/All Mail" . ?a)))
                   ))
          ))
   )
+
 ;;; UTILITIES
 (use-package go-translate
   :bind
@@ -324,6 +332,11 @@ emphasis markers inside of org mode"
   (interactive)
   (async-shell-command "yay -Syu"))
 (keymap-global-set "C-c C-u" 'gp/arch-update)
+
+(defun gp/arch-install (program)
+  "Runs the Yay shell command to install the inputted program"
+  (interactive "DProgram Name: ")
+  (async-shell-command (format "sudo pacman -S %s" program)))
 
 ;;; THEMEING
 ;; This is at the end of the config because it is the least important
