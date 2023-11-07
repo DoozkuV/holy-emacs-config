e3503946935fba728ddd8c5e3e470f80dfc75f66
George Padron <georgenpadron@gmail.com>
2023-11-07 18:11:00 +0100

    Refactored arch programs

--- productivity/arch.el
+++ productivity/arch.el
@@ -9,19 +9,18 @@
   "Runs the Yay shell command to automatically update the system on Arch Linux"
   (interactive)
   (async-shell-command "yay -Syyu"))
-(keymap-global-set "C-c C-u" 'gp/arch-update)
 
 ;;;###autoload
 (defun gp/arch-install (program)
   "Runs the Yay shell command to install the inputted program"
   (interactive "MProgram Name: ")
-  (async-shell-command (format "sudo pacman -S %s" program)))
+  (async-shell-command (concat "yay -S " program)))
 
 ;;;###autoload
 (defun gp/arch-uninstall (program)
   "Runs the Yay shell command to uninstall the inputted program"
   (interactive "MProgram Name: ")
-  (async-shell-command (format "sudo pacman -Rns %s" program)))
+  (async-shell-command (concat "yay -Rns " program)))
 
 (keymap-global-set "C-c a u" 'gp/arch-update)
 (keymap-global-set "C-c a i" 'gp/arch-install)
