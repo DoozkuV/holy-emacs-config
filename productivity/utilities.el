39fe9e91f4e7f8defe2f577caa6400faa79948d9
George Padron <georgenpadron@gmail.com>
2023-11-12 22:57:24 +0100

    Added repeat mode and removed unnecessary functionality

--- productivity/utilities.el
+++ productivity/utilities.el
@@ -2,6 +2,10 @@
 (recentf-mode 1) ; Enable file history
 (keymap-global-set "C-c C-r" 'recentf)
 
+;;; REPEAT MODE
+;; Allows one to repeat certain commands 
+(repeat-mode 1)
+
 ;;; STARTUP BUFFER
 (setq inhibit-splash-screen t
       initial-buffer-choice nil)
@@ -65,33 +69,3 @@
   (mapcar 'kill-buffer (buffer-list))
   (delete-other-windows))
 (keymap-global-set "C-c C-k" 'gp/kill-all-buffers)
-
-;;; WINDOW COMMANDS
-;; The following function runs any command in another buffer
-;;;###autoload
-(defun gp/run-command-other-buffer (command)
-  "Runs a command that returns a buffer, and then opens that buffer in another window"
-  (interactive
-   (list  (intern (read-extended-command))))
-  (cond
-   ((null command)
-    (user-error "No command is bound to %s"
-		(key-description key-sequence)))
-   ((commandp command)
-    (let ((buf (call-interactively command)))
-      (switch-to-buffer (other-buffer buf))
-      (switch-to-buffer-other-window buf)))
-   (t
-    (user-error "%s is bound to %s which is not a command"
-		(key-description key-sequence)
-		command))))
-
-(defun gp/run-sequence-other-buffer (key-sequence)
-  "Runs a command that returns a buffer, and then opens that buffer in another window"
-  (interactive
-   (list (read-key-sequence "Press key: ")))
-  (gp/run-command-other-buffer (key-binding key-sequence)))
-
-;; Set keymap here to C-x 7
-(keymap-global-set "C-x 7 k" 'gp/run-sequence-other-buffer)
-(keymap-global-set "C-x 7 f" 'gp/run-command-other-buffer) 
